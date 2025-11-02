318a96b68d298fe5cbf6dd36681927584c8d9e3a
axelalvarez84 <axelalvarez84@gmail.com>
2025-11-02 13:40:59 -0600

    Updated index.ts

--- supabase/functions/create-checkout-session/index.ts
+++ supabase/functions/create-checkout-session/index.ts
@@ -148,7 +148,7 @@ serve(async (req) => {
     // Create a checkout session
     const session = await stripe.checkout.sessions.create({
       customer: customerId,
-      automatic_payment_methods: { enabled: true },
+      payment_method_types: ['card'],
       line_items: [
         {
           price_data: {
