a7a053c63fdd836006560c65b079bbae4c9d4b83
axelalvarez84 <axelalvarez84@gmail.com>
2025-12-18 18:55:35 -0600

    Added 20251219005450_add_mascota_to_categoria_viajero_constraint.sql

---
+++ supabase/migrations/20251219005450_add_mascota_to_categoria_viajero_constraint.sql
@@ -0,0 +1,16 @@
+/*
+  # Agregar 'mascota' al constraint de categoria_viajero
+
+  1. Cambios
+    - Eliminar el constraint existente
+    - Crear nuevo constraint que incluya 'mascota'
+*/
+
+-- Eliminar el constraint existente
+ALTER TABLE booking_travelers 
+DROP CONSTRAINT IF EXISTS booking_travelers_categoria_viajero_check;
+
+-- Crear nuevo constraint con 'mascota' incluida
+ALTER TABLE booking_travelers 
+ADD CONSTRAINT booking_travelers_categoria_viajero_check 
+CHECK (categoria_viajero = ANY (ARRAY['infante'::text, 'nino'::text, 'adulto'::text, 'adulto_mayor'::text, 'mascota'::text]));
