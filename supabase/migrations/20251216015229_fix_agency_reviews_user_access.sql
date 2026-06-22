4b0bd6573a3e2af293e4f461d186d27453d4f434
ToursRed <axelalvarez84@gmail.com>
2026-06-22 17:57:49 -0600

    Update 20251216015229_fix_agency_reviews_user_access.sql

--- supabase/migrations/20251216015229_fix_agency_reviews_user_access.sql
+++ supabase/migrations/20251216015229_fix_agency_reviews_user_access.sql
@@ -1,16 +1,3 @@
-/*
-  # Fix Agency Reviews User Access
-
-  1. Changes
-    - Add policy to allow reading basic user information (first_name, last_name) 
-      when accessed through public agency reviews
-    - This allows anyone to see reviewer names on agency review pages
-
-  2. Security
-    - Only exposes first_name and last_name (no sensitive data)
-    - Only applies when the user has written a visible agency review
-    - Maintains privacy for all other user data
-*/
 
 -- Create a policy that allows reading basic user info for users who have written agency reviews
 CREATE POLICY "Anyone can view basic info of users who wrote agency reviews"
