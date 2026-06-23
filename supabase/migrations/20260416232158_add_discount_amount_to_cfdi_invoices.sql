c4e8f67df3ac47f8ed5fa6e2e74e3b5b0fe4ed44
ToursRed <axelalvarez84@gmail.com>
2026-06-22 20:12:04 -0600

    Update 20260416232158_add_discount_amount_to_cfdi_invoices.sql

--- supabase/migrations/20260416232158_add_discount_amount_to_cfdi_invoices.sql
+++ supabase/migrations/20260416232158_add_discount_amount_to_cfdi_invoices.sql
@@ -1,11 +1,3 @@
-/*
-  # Agregar columna discount_amount a cfdi_invoices
-
-  ## Cambios
-  - Nueva columna `discount_amount` (numeric, nullable) en la tabla `cfdi_invoices`
-    para almacenar el descuento total aplicado en la factura (con IVA incluido).
-    Se usa para reservas con códigos de descuento y para membresías con cupones de Stripe.
-*/
 
 DO $$
 BEGIN
