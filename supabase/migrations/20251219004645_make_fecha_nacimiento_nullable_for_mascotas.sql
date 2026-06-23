98bb98298007e5fe717cee21b780b5ae39eddd44
ToursRed <axelalvarez84@gmail.com>
2026-06-22 18:02:15 -0600

    Update 20251219004645_make_fecha_nacimiento_nullable_for_mascotas.sql

--- supabase/migrations/20251219004645_make_fecha_nacimiento_nullable_for_mascotas.sql
+++ supabase/migrations/20251219004645_make_fecha_nacimiento_nullable_for_mascotas.sql
@@ -1,10 +1,4 @@
-/*
-  # Hacer fecha_nacimiento nullable para mascotas
 
-  1. Cambios
-    - Cambiar la columna `fecha_nacimiento` en `booking_travelers` para que sea nullable
-    - Las mascotas no necesitan fecha de nacimiento
-*/
 
 ALTER TABLE booking_travelers 
 ALTER COLUMN fecha_nacimiento DROP NOT NULL;
