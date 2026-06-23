391e3c4e9498129cf5882ebcba7696f4c2454059
ToursRed <axelalvarez84@gmail.com>
2026-06-22 19:11:36 -0600

    Update 20260115042753_add_tour_code_to_inquiries.sql

--- supabase/migrations/20260115042753_add_tour_code_to_inquiries.sql
+++ supabase/migrations/20260115042753_add_tour_code_to_inquiries.sql
@@ -1,15 +1,3 @@
-/*
-  # Add tour_code field to international_tour_inquiries
-
-  1. Changes
-    - Add `tour_code` column to international_tour_inquiries table
-    - This field stores the Mega Travel tour code (e.g., MT-20293)
-    - Optional field that helps identify specific tours the customer is interested in
-
-  2. Notes
-    - The field is optional (nullable) as not all inquiries may have a tour code
-    - No need to create an index since this field won't be used for filtering
-*/
 
 -- Add tour_code column to international_tour_inquiries
 DO $$
