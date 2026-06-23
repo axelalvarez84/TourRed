fc23c901de590956070b6dc87ff52d8b8b7abe28
ToursRed <axelalvarez84@gmail.com>
2026-06-22 19:44:52 -0600

    Update 20260217034807_fix_commission_records_incorrect_status.sql

--- supabase/migrations/20260217034807_fix_commission_records_incorrect_status.sql
+++ supabase/migrations/20260217034807_fix_commission_records_incorrect_status.sql
@@ -1,15 +1,3 @@
-/*
-  # Fix incorrect commission_records status
-
-  1. Changes
-    - Update all commission_records with status='processed' but processed_at IS NULL
-    - These should have status='pending' since they haven't actually been paid
-    - Affects 49 records totaling $25,492.50 MXN
-    
-  2. Notes
-    - This fixes a bug where commissions were incorrectly marked as processed when created
-    - These commissions are actually pending payment to agencies
-*/
 
 UPDATE commission_records
 SET status = 'pending'
