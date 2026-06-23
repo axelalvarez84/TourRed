b68b54c06d3590b5b8e49410f494b094522f7852
ToursRed <axelalvarez84@gmail.com>
2026-06-22 20:01:49 -0600

    Update 20260314014624_add_tour_announcement_notification_type.sql

--- supabase/migrations/20260314014624_add_tour_announcement_notification_type.sql
+++ supabase/migrations/20260314014624_add_tour_announcement_notification_type.sql
@@ -1,8 +1,2 @@
-/*
-  # Add tour_announcement to notification_type enum
-
-  Adds a new notification type for agency-sent tour announcements/broadcasts.
-  Used when agencies send mass messages via the "Mensaje a Asistentes" feature.
-*/
 
 ALTER TYPE notification_type ADD VALUE IF NOT EXISTS 'tour_announcement';
