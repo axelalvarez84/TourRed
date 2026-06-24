b86793c96801ec5a2cebb8f75e8d20ccb2fd0c57
ToursRed <axelalvarez84@gmail.com>
2026-06-23 22:03:36 -0600

    Grant execute permission for roles

---
+++ supabase/migrations/20260624040240_grant_execute_permission_functions_to_authenticated.sql
@@ -0,0 +1,4 @@
+
+GRANT EXECUTE ON FUNCTION public.has_manage_travelers_permission() TO authenticated;
+GRANT EXECUTE ON FUNCTION public.has_manage_messages_permission() TO authenticated;
+GRANT EXECUTE ON FUNCTION public.has_permission(text) TO authenticated;
