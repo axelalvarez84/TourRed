543eeab661a58ae7cc3de3dcb99355a50f092c63
ToursRed <axelalvarez84@gmail.com>
2026-06-25 16:43:55 -0600

    Updated MaintenancePage.tsx

--- src/pages/MaintenancePage.tsx
+++ src/pages/MaintenancePage.tsx
@@ -16,7 +16,7 @@ const MaintenancePage: React.FC<MaintenancePageProps> = ({
           <img
             src="/LogoFinal.jpg"
             alt="ToursRed"
-            className="h-11 rounded-xl shadow-2xl"
+            className="h-21 rounded-xl shadow-2xl"
             onError={(e) => {
               (e.target as HTMLImageElement).style.display = 'none';
             }}
